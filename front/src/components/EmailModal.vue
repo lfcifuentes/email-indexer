bb13b7c37a7891d951185871ef5862bc3bddd8c9
luis cifuentes <lfcifuentes28@gmail.com>
2025-02-09 23:13:23 -0500

    feat: update EmailModal styles for improved background and structure

--- front/src/components/EmailModal.vue
+++ front/src/components/EmailModal.vue
@@ -1,9 +1,8 @@
 <template>
   <div
     tabindex="-1"
-    class="flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full h-screen backdrop-blur-[2px]"
+    class="flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 max-h-full h-screen backdrop-blur-[2px] bg-black/50 "
     @click.self="closeEmailModal"
-    style="background-color: rgba(0, 0, 0, 0.5)"
   >
     <div class="relative p-4 w-full max-w-[700px] max-h-full">
       <!-- Modal content -->
@@ -75,3 +74,6 @@ const closeEmailModal = () => {
 }
 
 </script>
+
+<style scoped>
+</style>
