02497f594fa19d59be67b277465149ea6dd8ab80
luis cifuentes <lfcifuentes28@gmail.com>
2025-02-09 23:18:09 -0500

    feat: update Pagination component styles for improved dark mode support

--- front/src/components/Pagination.vue
+++ front/src/components/Pagination.vue
@@ -5,7 +5,7 @@
         <button
           :disabled="meta?.current_page === 1"
           @click="clickButton({ label: 1 })"
-          class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-t-purple dark:hover:text-white"
           :class="{
             'cursor-not-allowed': meta?.current_page === 1,
             'cursor-pointer': meta?.current_page !== 1
@@ -18,7 +18,7 @@
         <button
           :disabled="meta?.current_page === 1"
           @click="clickButton(prevPage)"
-          class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-t-purple dark:hover:text-white"
           :class="{
             'cursor-not-allowed': meta?.current_page === 1,
             'cursor-pointer': meta?.current_page !== 1
@@ -32,8 +32,8 @@
           v-if="link.index !== ''"
           class="flex items-center justify-center px-3 h-8 leading-tight border"
           :class="{
-            'text-blue-600 border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-500 dark:hover:text-white': link.active,
-            'text-gray-500 bg-white border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white cursor-pointer' : !link.active
+            'text-white border-gray-300 bg-t-purple hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-t-light-purple dark:text-white dark:hover:bg-t-purple dark:hover:text-white': link.active,
+            'text-gray-500 bg-white border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-t-purple dark:hover:text-white cursor-pointer' : !link.active
           }"
           @click="clickButton(link)"
         >
@@ -44,7 +44,7 @@
         <button
           :disabled="meta?.current_page === meta.last_page"
           @click="clickButton(nextPage)"
-          class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-t-purple dark:hover:text-white"
           :class="{
             'cursor-not-allowed': meta?.current_page === meta.last_page,
             'cursor-pointer': meta?.current_page !== meta.last_page
@@ -57,7 +57,7 @@
         <button
           :disabled="meta?.current_page === meta.last_page"
           @click="clickButton({ label: meta.last_page })"
-          class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-t-purple dark:hover:text-white"
           :class="{
             'cursor-not-allowed': meta?.current_page === meta.last_page,
             'cursor-pointer': meta?.current_page !== meta.last_page
